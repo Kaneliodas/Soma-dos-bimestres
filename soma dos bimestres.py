b93ff94cb1e3b1d2a4883bf901fb8febaa0d0e7a
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-28 17:09:21 -0300

    Create soma dos bimestres.py

---
+++ soma dos bimestres.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+n1 = float(input("Digite sua nota no 1° bimestre: "))
+n2 = float(input("Digite sua nota no 2° bimestre: "))
+n3 = float(input("Digite sua nota no 3° bimestre: "))
+n4 = float(input("Digite sua nota no 4° bimestre: "))
+
+soma = str((n1+n2+n3+n4)/4)
+
+print("A sua nota geral é: " + soma)
